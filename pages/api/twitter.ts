f9ddd78a4e56cd5f53407e87e4a056cfb1215c12
mikana0918 <shogo.kusuhara@gmail.com>
2021-10-29 17:27:24 +0900

    feat: :sparkles: tweetのget API作成

---
+++ pages/api/twitter.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { string } from 'fp-ts'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  icon: string;
+  tweet: string;
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
+) {
+  const data = [
+    {
+      icon: "https://pbs.twimg.com/profile_images/1255543381347852288/LKtzr5w9_400x400.png",
+      tweet: "テストテスト1"
+    },
+    {
+      icon: "https://pbs.twimg.com/profile_images/1255543381347852288/LKtzr5w9_400x400.png",
+      tweet: "テストテスト2"
+    },
+    {
+      icon: "https://pbs.twimg.com/profile_images/1255543381347852288/LKtzr5w9_400x400.png",
+      tweet: "テストテスト3"
+    }
+  ]
+  res.status(200).json(data)
+}
